35471b201750632dbbf41643286c7ca8b3b5b012
Hasnah Ngah <hasnah@Hasnahs-MacBook-Air.local>
2020-04-28 14:53:13 +0800

    adding routing codes in routes files

---
+++ api/routes/projects.js
@@ -0,0 +1,6 @@
+
+module.exports = function (router) {
+  router.get('/projects', (req, res) => {
+
+  });
+};
---
+++ api/routes/standup.js
@@ -0,0 +1,8 @@
+
+
+module.exports = function (router) {
+    router.get('/standup', (req, res) => {
+  
+    });
+  };
+  
